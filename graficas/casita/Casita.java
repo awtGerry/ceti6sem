9a0432eb50181f9ee1410655b11e1a76baf53008
Victor Rodriguez <awtgerry@gmail.com>
2023-08-19 10:07:24 -0600

    Casa

--- graficas/casita/Casita.java
+++ graficas/casita/Casita.java
@@ -1,43 +1,42 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.GridLayout;
 import java.awt.Graphics;
 
 import javax.swing.*;
 
 public class Casita extends JFrame {
-    private JFrame frame = new JFrame();
-    private JPanel panel = new JPanel();
-    private JLabel label = new JLabel();
 
     public Casita() {
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(label);
-
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        // frame.setVisible(true);
+        setSize(1366, 768);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
         Casita casita = new Casita();
         casita.setVisible(true);
-        // new Casita();
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        // circulo, cuadrado, rectangulo
-        g.drawOval(100, 100, 100, 100);
-        g.fillOval(100, 100, 100, 100);
-        g.setColor(Color.BLUE);
-        g.drawRect(100, 200, 100, 100);
-        g.fillRect(100, 200, 100, 100);
-        g.setColor(Color.RED);
-        g.drawRect(100, 300, 200, 100);
-        g.fillRect(100, 300, 200, 100);
+        // BACKGROUND
         g.setColor(Color.GREEN);
+        g.fillRect(0, 320, 1366, 400);
+        g.setColor(Color.CYAN);
+        g.fillRect(0, 0, 1366, 368);
+        // SUN
+        g.setColor(Color.YELLOW);
+        g.fillOval(1160, 50, 100, 100);
+        // HOUSE
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(600, 400, 200, 200);
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillPolygon(new int[] { 600, 700, 800 }, new int[] { 400, 300, 400 }, 3);
+        g.setColor(new Color(102, 51, 0));
+        g.fillRect(670, 500, 60, 100);
+        // Tree
+        g.setColor(new Color(102, 51, 0));
+        g.fillRect(100, 400, 60, 200);
+        g.setColor(new Color(53, 94, 59));
+        g.fillOval(50, 300, 150, 150);
     }
+
 }
