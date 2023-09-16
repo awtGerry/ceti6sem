ba675cd08e0fdfe237b4a4c97bc220b62fc18c3d
Victor Rodriguez <awtgerry@gmail.com>
2023-09-16 17:55:25 -0600

    Buildings done.
    TODO:
        - Change the color of the buildings to black
        - Change the background to a sky texture
        - Add the moon
        - Add the clock and his functionallity
        - Add the starts and do a spawn thingy.
        - Add peter pan characters

--- graficas/1p/engine/clock/src/figures.rs
+++ graficas/1p/engine/clock/src/figures.rs
@@ -26,10 +26,10 @@ pub fn draw_pixel(x: i32, y: i32) {
         gl::DrawArrays(gl::POINTS, 0, 2);
         gl::Enable(gl::SCISSOR_TEST);
         gl::Scissor(x, y, 1, 1);
-        gl::ClearColor(1.0, 1.0, 1.0, 1.0);
-        gl::Clear(gl::COLOR_BUFFER_BIT);
+        // gl::ClearColor(0.0, 0.0, 1.0, 0.0);
+        // gl::Clear(gl::COLOR_BUFFER_BIT);
         gl::Disable(gl::SCISSOR_TEST);
-        gl::DrawArrays(gl::LINES, 0, 2);
+        gl::DrawArrays(gl::LINES, 0, 5);
     }
 }
 
@@ -52,18 +52,35 @@ pub fn draw_line(x1: f32, y1: f32, x2: f32, y2: f32) { // DDA
 
     for _ in 0..steps as i32 {
         draw_pixel(x as i32, y as i32);
+        unsafe {
+            gl::DrawArrays(gl::LINES, 0, 5);
+        }
         x += x_inc;
         y += y_inc;
     }
 }
 
-pub fn draw_triangle(x1: f32, y1: f32, x2: f32, y2: f32) {
-    let vertices: [f32; 18] = [
-        // positions        // colors
-        x1, y1, 0.0,        1.0, 0.0, 0.0, // bottom right
-        x2, y1, 0.0,        0.0, 1.0, 0.0, // bottom left
-        0.0, y2, 0.0,        0.0, 0.0, 1.0, // top
+pub fn draw_triangle(x1: f32, y1: f32, x2: f32, y2: f32, x3: f32) {
+    let vertices: [f32; 9] = [
+        // Positions
+        x1, y1, 0.0,
+        x2, y1, 0.0,
+        x3, y2, 0.0,
     ];
+    let indices: [u32; 3] = [
+        0, 1, 2,
+    ];
+
+    let vao = Vao::new();
+    let vbo = Buffer::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+    set_vao_vbo(&vao, &vbo, &vertices, 3);
+    let ebo = Buffer::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
+    ebo.bind();
+    ebo.buffer_data(&indices);
+
+    unsafe {
+        gl::DrawElements(gl::TRIANGLES, 3, gl::UNSIGNED_INT, ptr::null());
+    }
 }
 
 
@@ -85,6 +102,32 @@ pub fn draw_rectangle_fill(x1: f32, y1: f32, x2: f32, y2: f32) {
         x2, y1, 0.0,
         x1, y2, 0.0,
         x2, y2, 0.0,
+    ];
+    let indices: [u32; 6] = [
+        0, 1, 2,
+        1, 2, 3,
+    ];
+
+    let vao = Vao::new();
+    let vbo = Buffer::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+    set_vao_vbo(&vao, &vbo, &cordenates, 3);
+    let ebo = Buffer::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
+    ebo.bind();
+    ebo.buffer_data(&indices);
+
+    unsafe {
+        gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+    }
+}
+
+// This will draw the roof of the tower, is a bit more complex so it has its own function
+pub fn draw_roof(x1: f32, y1: f32, x2: f32, y2: f32, x3: f32, x4: f32) {
+    let cordenates: [f32; 12] = [
+        // Positions
+        x1, y1, 0.0,
+        x2, y1, 0.0,
+        x3, y2, 0.0,
+        x4, y2, 0.0,
     ];
     let indices: [u32; 6] = [
         0, 1, 2,
