1dc6fa3c743f10c33d8edf775bc18c82edec4ba5
Victor Rodriguez <awtgerry@gmail.com>
2023-09-30 15:00:48 -0600

    Added all algorithms to root

--- graficas/1p/engine/src/lib.rs
+++ graficas/1p/engine/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod graphics;
 pub mod algorithms;
+
+pub use ::gl::types::*;
--- graficas/1p/engine/testing/src/main.rs
+++ graficas/1p/engine/testing/src/main.rs
@@ -1,27 +1,23 @@
 use engine::graphics::window::Window;
 
-mod figures;
+use engine::algorithms::figures::fill_rectangle;
 
-use std::ptr;
-
-const WIDTH: u32 = 1000;
-const HEIGHT: u32 = 768;
-const R: f32 = 80.0;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
 
 fn main() {
-    let mut window = Window::new(WIDTH, HEIGHT, "Class testing");
+    let mut window = Window::new(WIDTH, HEIGHT, "Title");
 
     window.init();
+
     while !window.should_close() {
+
         unsafe {
-            gl::ClearColor(0.0, 0.0, 0.0, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-            figures::draw_rectangle(0.5, 0.5, -0.5, -0.5);
-            figures::scan_line(-0.5, 0.5);
-            figures::draw_circle_fill(165.0, 120.0, R);
-            // gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+            gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+            fill_rectangle(-0.5, -0.5, 0.5, 0.5);
             gl::DrawArrays(gl::LINES, 0, 5);
         }
+
         window.update();
     }
 }
